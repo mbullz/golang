9d7a942eef306adba65bc408c3ffc23d46a424c3
Robert Tio <m_bull_z@yahoo.com>
2017-01-27 17:40:15 +0700

    update

--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("Hello World 2")
+	fmt.Println("hello")
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello!"))
+}
+
+func Show(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Show a List of Customer!"))
+}
+
+func Server() {
+	http.HandleFunc("/", Hello)
+	http.HandleFunc("/show", Show)
+	fmt.Println("Listening on Port :8080 ...")
+	http.ListenAndServe(":8080", nil)
 }
